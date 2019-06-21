042b14ae9e2eae2862360864b396a35815f6eace
Michael Jackson <michael@jackson.us>
2019-06-21 07:52:01 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+Title: Composing Behavior in React or Why React Hooks are Awesome
+
+- Who am I?
+- What does "composition" mean?
+  - Discrete behaviors
+  - May be combined
+  - Shared interface is the key
+  - Example: CLI tools
+- What is "function composition"?
+  - Shared interface is arguments + return value
+  - Example: `add` function
+- React Hooks are functions with superpowers
+  - They can manage state
+  - They can participate in the lifecycle
+  - They can receive implicit arguments (via context)
+  - But the best feature of all? Composition!
+    - They are **just functions**, after all
+- Custom Hooks
+  - useLogging in \<Minutes> (single behavior)
+  - usePersistentState in \<NewPost> (single state + behavior)
+  - useUndo in \<NewPost> (ref + state + behavior + method override)
+- Hooks as useReducer Middleware
+  - useLogger in \<Feed>
+  - useThunk in \<Feed>
---
+++ hooks.js
@@ -0,0 +1,92 @@
+import { useEffect, useRef, useState } from 'react';
+
+export function useLogging(message) {
+  useEffect(() => {
+    console.log(message);
+  }, [message]);
+}
+
+function getLocalStorageValue(key) {
+  const val = localStorage.getItem(key);
+  if (!val) return null;
+  try {
+    return JSON.parse(val);
+  } catch (e) {
+    return null;
+  }
+}
+
+function setLocalStorage(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+export function usePersistentState(key, defaultState = '') {
+  const [state, setState] = useState(getLocalStorageValue(key) || defaultState);
+
+  useEffect(() => {
+    setLocalStorage(key, state);
+  });
+
+  return [state, setState];
+}
+
+export function useUndo([state, setState]) {
+  const history = useRef([state]);
+  const [index, setIndex] = useState(0);
+
+  function undo() {
+    setIndex(Math.max(0, index - 1));
+  }
+  function redo() {
+    setIndex(Math.min(history.current.length - 1, index + 1));
+  }
+  function newSetState(nextState) {
+    const nextIndex = index + 1;
+    // Truncate any future redos.
+    history.current = history.current.slice(0, nextIndex);
+    history.current[nextIndex] = nextState;
+    setIndex(nextIndex);
+    setState(nextState);
+  }
+
+  return [history.current[index], newSetState, undo, redo];
+}
+
+export function useLogger([state, dispatch]) {
+  const actionRef = useRef();
+
+  const newDispatchRef = useRef(action => {
+    actionRef.current = action;
+    dispatch(action);
+  });
+
+  useEffect(() => {
+    const action = actionRef.current;
+
+    if (action) {
+      console.group();
+      console.log('Action:', action);
+      console.log('State:', state);
+      console.groupEnd();
+    }
+  }, [state]);
+
+  return [state, newDispatchRef.current];
+}
+
+export function useThunk([state, dispatch]) {
+  const stateRef = useRef();
+  stateRef.current = state;
+
+  const getStateRef = useRef(() => stateRef.current);
+
+  const newDispatchRef = useRef(action => {
+    if (typeof action === 'function') {
+      action(newDispatchRef.current, getStateRef.current);
+    } else {
+      dispatch(action);
+    }
+  });
+
+  return [state, newDispatchRef.current];
+}
